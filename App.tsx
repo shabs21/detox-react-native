28187a17dea4b2de3f4c1c68c47c4f29cef47ad7
shabs21 <shabhari@stackbox.xyz>
2024-07-16 16:45:34 +0530

    perf: cucumber integration working

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text testID="welcome">Welcome to Detox</Text>
+      {/* <Text testID="welcome">Welcome to Detox</Text> */}
+      <Text testID="welcome">Open up App.js to start working on your app!</Text>
       <Button title="Click Me" onPress={() => {}} testID="clickMeButton" />
     </View>
   );
